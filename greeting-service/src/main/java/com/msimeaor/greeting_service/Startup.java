a874e165466315eb5c1681abe7e53bf65a916e79
Matheus <maatsimeao@gmail.com>
2025-09-15 09:49:28 -0300

    some changes

---
+++ greeting-service/src/main/java/com/msimeaor/greeting_service/Startup.java
@@ -0,0 +1,16 @@
+package com.msimeaor.greeting_service;
+
+import com.msimeaor.greeting_service.config.GreetingConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(GreetingConfiguration.class)
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
